25c714438ace57027c26a902ec58defe2f7c0916
Daria Katkova <d.s.katkova@yandex.ru>
2023-11-02 02:52:53 +0300

    #Реализовано тестовое задание с версткой

--- app/src/main/java/ru/example/project1/MainActivity.kt
+++ app/src/main/java/ru/example/project1/MainActivity.kt
@@ -2,10 +2,49 @@ package ru.example.project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import ru.example.project1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.itemFlowerFirst.root.setOnClickListener {
+            Toast.makeText(this@MainActivity, "First", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.itemFlowerSecond.apply {
+            ivImage.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.ic_plant_yellow
+                )
+            )
+            tvName.text = getString(R.string.tulip)
+            root.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Second", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.itemFlowerThird.apply {
+            ivImage.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.ic_plants_cactus
+                )
+            )
+            tvName.text = getString(R.string.cactus)
+            root.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Third", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
